76c0f88d30d59d4331db30fbb2aafdb71d6951f3
kevinerfootballtheblonde <esposito02andrea@gmail.com>
2024-08-26 17:24:29 +0200

    astronave

--- astronave.py
+++ astronave.py
@@ -1,19 +1,25 @@
 import pygame, sys
 from pygame.locals import *
 
-class razzo:
-    def __init__(self, screen, pos, size, color,velocity) -> None:
+class astronave:
+    def __init__(self, screen, pos, size, color) -> None:
         self.screen=screen
         self.image=pygame.Surface(size)
         self.image.fill(color)
         self.rect=pygame.Rect(pos[0],pos[1],size[0],size[1])
         self.size=size
-        self.velocity=velocity
-        self.pos=pos
-    def move(self):
-        self.rect.y+=self.velocity
+    def moveleft(self,pixels):
+        if self.rect.x-pixels<=0:
+            self.rect.x=0
+        else:
+            self.rect.x-=pixels
+    def moveright(self,pixels):
+        if self.rect.right+pixels>=1100:
+            self.rect.x=1100-self.rect.size[0]
+        else:
+            self.rect.x+=pixels
     def draw(self):
-        term=pygame.image.load("rocket.png")
+        term=pygame.image.load("spaceship_red.png")
         term=pygame.transform.scale(term,(self.rect.width,self.rect.height))
         self.image=pygame.Surface((self.rect.width,self.rect.height))
         self.image.blit(term,(0,0))
--- sfondo.py
+++ sfondo.py
@@ -1,19 +1,18 @@
 import pygame, sys
 from pygame.locals import *
 
-class razzo:
-    def __init__(self, screen, pos, size, color,velocity) -> None:
+class sfondo:
+    def __init__(self, screen, pos, size, color) -> None:
         self.screen=screen
         self.image=pygame.Surface(size)
         self.image.fill(color)
         self.rect=pygame.Rect(pos[0],pos[1],size[0],size[1])
         self.size=size
-        self.velocity=velocity
         self.pos=pos
     def move(self):
-        self.rect.y+=self.velocity
+        self.rect.y+=5
     def draw(self):
-        term=pygame.image.load("rocket.png")
+        term=pygame.image.load("space.png")
         term=pygame.transform.scale(term,(self.rect.width,self.rect.height))
         self.image=pygame.Surface((self.rect.width,self.rect.height))
         self.image.blit(term,(0,0))
